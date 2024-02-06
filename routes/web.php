12d61835634800cf124e311b5dac025cf0aff731
Stirbet Mihail <mihaistirbet98@gmail.com>
2024-02-06 11:14:34 +0200

    Relatii, migrare

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PhoneController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::redirect('/', '/users');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get( '/users' , [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create' , [UserController::class , 'create'])->name('users.create');
+Route::post('/users' , [UserController::class , 'store'])->name('users.store');
+
+Route::controller('PhoneController::class')->group(function(){
+    Route::get('phones', 'index');
+    Route::get('phones/create', 'create');
+    Route::get('phones', 'store');
+    Route::get('phones/{phone}', 'show');
+    Route::get('phones/{phone}/edit', 'edit');
+    Route::get('phones/{phone}/update', 'update');
+    Route::get('phones/{phone}', 'destroi');
 });
